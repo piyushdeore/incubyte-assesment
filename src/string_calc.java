29e201852d6f2f05b0fca7b21e66dec0e8fd7d27
Piyush Deore <piyushyd2504@gmail.com>
2022-09-08 03:44:19 +0530

    second commit

--- src/string_calc.java
+++ src/string_calc.java
@@ -1,64 +1,44 @@
- 
-
 
 public class string_calc {
 
-	public static int add(String str)
-	{
-		if(str.equals("")){
+	public static int add(String str) {
+		if (str.equals("")) {
 			return 0;
-		}
-		else
-		{
-			String []arr = splitNumbers(str,",");
+		} else {
+			String[] arr = splitNumbers(str, ",");
 			return sum(arr);
 		}
 	}
-	
-	
-	public static String[] splitNumbers(String numbers, String divider){
-	    return numbers.split(divider);
+
+	public static String[] splitNumbers(String numbers, String divider) {
+		return numbers.split(divider);
 	}
 
-	public static int toInt(String number)
-	{
+	public static int toInt(String number) {
 		return Integer.parseInt(number);
 	}
 
-	
-	private static int sum(String[] mystr){
-		int tot=0;
- 	    if(mystr.length==1)
- 	    {
- 	    	tot=toInt(mystr[0]);
- 	    }
- 	    else if(mystr.length==2)
- 	    {
- 	    	tot=toInt(mystr[0])+toInt(mystr[1]);
- 	    }
- 	    return tot;
-		
+	private static int sum(String[] mystr) {
+		int total = 0;
+
+		for (String number : mystr) {
+
+			if (toInt(number) < 1000)
+				total += toInt(number);
 		}
 
-		
-    
-    public static void main( String[] args)
-    {
-     
-    	
-    	
-      System.out.println(add("1"));
-      
-      
-      
-      
-      System.out.println(add("1,4"));
-      
-     
-      
-      System.out.println(add("1,2000"));
-      
-    }
+		return total;
+
+	}
+
+	public static void main(String[] args) {
+
+		System.out.println(add("1"));
+
+		System.out.println(add("1,4"));
+
+		System.out.println(add("1,2000"));
+
+	}
 
 }
-
